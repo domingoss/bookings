c8388b790c5ea5b65cb4597123b2cf8453457010
domingoss <sebastiaomachava417@gmail.com>
2020-05-16 14:54:12 +0200

    update

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _userIsAthenticated = false
+  
+  get userIsAutheticated (){
+    return this._userIsAthenticated;
+    }
+
+  constructor() { }
+
+  login(){
+    this._userIsAthenticated = true;
+  }
+
+  false(){
+    this._userIsAthenticated = false;
+  }
+}
